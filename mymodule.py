391a91e120e913b7b84a4c605a33a8bf1218417d
annarailton <annarailton@github.com>
2023-02-10 18:44:55 +0000

    Add more docstrings, external imports

--- mymodule.py
+++ mymodule.py
@@ -4,3 +4,13 @@ import requests
 def get_ip():
     """Get my current external IP."""
     return requests.get("https://icanhazip.com").text.strip()
+
+
+def get_ip_long_docstring():
+    """Get my current external IP.
+
+    This is a long docstring.
+
+    Yes, a really long docstring
+    """
+    return requests.get("https://github.com/annarailton/test-python-tools").text.strip()
--- test_mymodule.py
+++ test_mymodule.py
@@ -1,4 +1,4 @@
-from mymodule import get_ip
+from mymodule import get_ip, get_ip_long_docstring
 
 def test_get_ip_type():
     """The IP is expected to be a string."""
@@ -9,4 +9,16 @@ def test_get_ip_type():
 def test_get_ip_dots():
     """The IP(v4) is expected to have 4 dots."""
     ip = get_ip()
+    assert ip.count('.') == 3
+
+
+def test_get_ip_type2():
+    """The IP is expected to be a string."""
+    ip = get_ip_long_docstring()
+    assert isinstance(ip, str)
+
+
+def test_get_ip_dots2():
+    """The IP(v4) is expected to have 4 dots."""
+    ip = get_ip_long_docstring()
     assert ip.count('.') == 3
